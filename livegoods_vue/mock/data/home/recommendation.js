cb0e9f26dc47ad5e7951d169252e5b78a73b63ea
RickyWang1020 <xw1744@nyu.edu>
2022-09-22 02:04:00 -0400

    first commit

---
+++ livegoods_vue/mock/data/home/recommendation.js
@@ -0,0 +1,29 @@
+module.exports = {
+    status:200,
+    data: [
+        {
+            id: Math.random().toString().slice(2),
+            title: "上海落地灯",
+            img: "http://iwenwiki.com/api/livable/homehot/img_luodideng.png",
+            link: ""
+        },
+        {
+            id: Math.random().toString().slice(2),
+            title: "上海置物架",
+            img: "http://iwenwiki.com/api/livable/homehot/img_zhiwujia.png",
+            link: ""
+        },
+        {
+            id: Math.random().toString().slice(2),
+            title: "上海矮脚灯",
+            img: "http://iwenwiki.com/api/livable/homehot/img_aijiaodeng.png",
+            link: ""
+        },
+        {
+            id: Math.random().toString().slice(2),
+            title: "上海毛巾",
+            img: "http://iwenwiki.com/api/livable/homehot/img_maojin.png",
+            link: ""
+        }
+    ]
+}
